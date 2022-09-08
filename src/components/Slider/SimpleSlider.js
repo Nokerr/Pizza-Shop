6a959623faaa3df13acfe4f03c300b93f85b593f
Vladyslav <vladislavdubina60@gmail.com>
2022-09-08 22:52:03 +0300

    slyder img change

--- src/components/Slider/SimpleSlider.js
+++ src/components/Slider/SimpleSlider.js
@@ -1,7 +1,7 @@
-import slide_1 from '../../assets/slider-img/kombo-box_slider.jpg'
 import slide_2 from '../../assets/slider-img/slider-combo.jpg'
 import slide_3 from '../../assets/slider-img/slider-new-yorker.jpg'
 import slide_4 from '../../assets/slider-img/uah-slider.jpg'
+import slide_5 from '../../assets/slider-img/slyder-ny-thiker.jpg'
 import './Slider.scss'
 
 import Slider from "react-slick";
@@ -30,10 +30,6 @@ const SimpleSlider = () => {
                 margin: '0 auto',
                 marginTop: '10px'
             }}>
-
-                <div>
-                    <h3><img src={slide_1} alt="slide_1" /></h3>
-                </div>
                 <div>
                     <h3><img src={slide_2} alt="slide_2" /></h3>
                 </div>
@@ -43,7 +39,9 @@ const SimpleSlider = () => {
                 <div>
                     <h3><img src={slide_4} alt="slide_4" /></h3>
                 </div>
-
+                <div>
+                    <h3><img src={slide_5} alt="slide_4" /></h3>
+                </div>
             </Slider>
         </div >
     )
